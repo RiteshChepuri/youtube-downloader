2c43d7d3bdc9e02ecfd182f6f7bfadaff5e806b6
RiteshChepuri <riteshchepuri0@gmail.com>
2022-09-25 16:11:41 +0530

    Playlist mp4 format download added

--- main.py
+++ main.py
@@ -1,26 +1,58 @@
 from pytube import YouTube
+from pytube import Playlist
 import os 
 
-function = input("What do you want to do\n 1 for Video \n 2 for Audio\n")
+main_input = input("What do you want to do?\n 1 for Single Video \n 2 for Entire Playlist\n")
 
-if function == "1" :
-    link = input("Enter the link of video\n")
-    yt1 = YouTube(link)
-    print("Title: ", yt1.title)
-    vd = yt1.streams.get_highest_resolution()
-    vd.download('D:\\Personal\\Audios_and_Videos\\Video')
-    print(yt1.title + "Video downloaded succesfully!!!")
+def playlist_down():
+    playlist_link = input("Enter the link of the Playlist\n")
+    playlist = Playlist(playlist_link)
+    downloaddirectory = 'D:\\Personal\\Audios_and_Videos\\Playlist'
 
-if function == "2":
-    link = input("Enter the link of video\n")
-    yt2 = YouTube(link)
-    print("Title: ", yt2.title)
-    ad = yt2.streams.filter(only_audio=True).first()
-    dest = 'D:\\Personal\\Audios_and_Videos\\Audio'
-    out_file = ad.download(output_path=dest)
+    print("Total videos to download:" , len(playlist.video_urls))
+    print("\n\n Links of the youtube videos\n")
 
-    base,ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print(yt2.title + " Audio downloaded succesfully!!!")
+    for url in playlist.video_urls:
+        print(url)
+
+    for video in playlist.videos:
+        print('Downloading: {} with url: {}'.format(video.title, video.watch_url))
+        video.streams.\
+            filter(type='video', progressive=True, file_extension='mp4').\
+            order_by('resolution').\
+            desc().\
+            first().\
+            download(downloaddirectory)
+    print("Playlist download complete")
+
+
+if main_input == '2' :
+    playlist_down()
+
+
+if main_input == '1' :
+    function = input("What do you want to do\n a for Video \n b for Audio\n")
+
+    if function == "a" :
+        link = input("Enter the link of video\n")
+        yt1 = YouTube(link)
+        print("Title: ", yt1.title)
+        vd = yt1.streams.get_highest_resolution()
+        vd.download('D:\\Personal\\Audios_and_Videos\\Video')
+        print(yt1.title + "Video downloaded succesfully!!!")
+
+    if function == "b":
+        link = input("Enter the link of video\n")
+        yt2 = YouTube(link)
+        print("Title: ", yt2.title)
+        ad = yt2.streams.filter(only_audio=True).first()
+        dest = 'D:\\Personal\\Audios_and_Videos\\Audio'
+        out_file = ad.download(output_path=dest)
+
+        base,ext = os.path.splitext(out_file)
+        new_file = base + '.mp3'
+        os.rename(out_file, new_file)
+        print(yt2.title + " Audio downloaded succesfully!!!")
+
+
 
